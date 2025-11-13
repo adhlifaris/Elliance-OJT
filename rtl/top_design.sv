62f9ea547a080479421f4ade25ba993cdf5259d6
adhlifaris <adhlifaris@gmail.com>
2025-11-13 11:17:40 +0800

    Initial commit

---
+++ rtl/top_design.sv
@@ -0,0 +1,34 @@
+
+
+///////////
+//Central Control Fabric
+//////////
+
+//////////////
+// Config and Control Subsystem
+//////////////
+
+//////////
+// Compute Cluster Array
+/////////
+
+////////
+//Memory Subsystem
+///////
+
+////////
+//Data Movement Engine
+////////
+
+///////
+//Power Management Subsystem
+//////
+
+//////
+//External Imterface
+/////
+
+////
+//Monitoring and Telemetry
+///
+
